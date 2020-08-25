c1170c74c072aab3d57b426bf0cdb9aa4c09f1c4
lvasellati <lvasellati@gmail.com>
2020-08-25 20:50:57 -0300

    Impresion Acta

--- src/app/actas/actas.component.ts
+++ src/app/actas/actas.component.ts
@@ -96,148 +96,122 @@ export class ActasComponent implements OnInit {
   }
 
   pdfGenerator(){
-  
-    var doc = new jsPDF('p','mm','A4');
-    doc.page=1;
-    var pagina = "Pagina: " + doc.page ;
-    doc.setFontSize(30);
-    doc.text(80,10,'ORIGINAL')
+
+
+    var versiones = ['ORIGINAL','DUPLICADO','DIV REGISTRACIONES','TRANSPORTE','FISCALIZACION']; 
+    var doc=Array;
+    for (var i=0;i<versiones.length;i++){
     
-    //doc.text(pagina, 50, doc.internal.pageSize.height );
+    //var ver:string=versiones[i];
+
+    //console.log(ver+' '+versiones.length);    }
+
+    doc[i] = new jsPDF('p','mm','A4');
+    //var doc[i]2 = new jsPDF('p','mm','A4');
+    doc[i].page=1;
+    var contacto:string;
+    var obvs:string;
+    var items:number=this.remito.length; 
+    var direccion:string="."
+    var startY:number=55;
+    var pagina = "Pagina: " + doc[i].page ;
+    
+    //doc[i].setFontSize(30);
+    //doc[i].text(80,10,ver)
+    
+    //doc[i].text(pagina, 50, doc[i].internal.pageSize.height );
     var cuerpo = [];
     var imgData = "../../assets/logo13.png"
+    var imgData1 = "../../assets/blanco.png"
     //Cabecera
-    var contacto:string;
-    var items:number=this.remito.length;
     
-    doc.setFontSize(15);
-    doc.text(170,10,'Acta n°: '+ this.nRemito);
+    //for (var i=0;i<versiones.length;i++){
+    
+    doc[i].setFontSize(15);
+    doc[i].text(170,10,'Acta n°: '+ this.nRemito);
 
-    doc.setFontSize(10);
+    doc[i].setFontSize(10);
     var dia:string=(this.remito[0].Fecha.toString().substring(8,10)+this.remito[0].Fecha.toString().substring(4,8)+this.remito[0].Fecha.toString().substring(0,4));
-    doc.text(10,10, 'Fecha: '+ dia);
-    //doc.text(10,10, 'Fecha: '+ this.remito[0].Fecha.toString().substring(0,10));
-    doc.text(10,20,'De: '+ this.remito[0].De);
-    doc.text(10,30,'Para: '+ this.remito[0].Para);
+    doc[i].text(10,10, 'Fecha: '+ dia);
+    //doc[i].text(10,10, 'Fecha: '+ this.remito[0].Fecha.toString().substring(0,10));
+    doc[i].text(10,20,'De: '+ this.remito[0].De);
+    doc[i].text(10,27,'Para: '+ this.remito[0].Para);
+    direccion = "Direccion: "+ this.remito[0].Direccion + "    Localidad: "+ this.remito[0].Localidad + "."+ this.remito[0].Provincia;
+    doc[i].text(10,32,direccion);
     
     if(this.remito[0].Contacto==null){this.contacto=' . ' ;}
     else{this.contacto=this.remito[0].Contacto;}
-    doc.text(10,40,'Contacto: '+ this.contacto);
+    doc[i].text(10,40,'Contacto: '+ this.contacto);
     
-    if(this.remito[0].Observaciones==null){this.contacto=' . ' ;}
-    else{this.contacto=this.remito[0].Observaciones;}
-    doc.text(10,50,'Observaciones: ' + this.contacto);
-    doc.addImage(imgData, 'png',140 , 15, 50, 30);
+    if(this.remito[0].Observaciones==null){obvs=' . ' ;}
+    else{obvs=this.remito[0].Observaciones;}
+    //observaciones
+    doc[i].text(10,50,'Observaciones: ' + obvs.toString().substring(0,80));
+    if(obvs.length> 80){doc[i].text(36,55, obvs.toString().substring(80,160));
+    startY=startY+5;
+    if(obvs.length>160){doc[i].text(36,60,obvs.toString().substring(160,240));
+    startY=startY+5;}
+    }
+    
+        
+    doc[i].addImage(imgData, 'png',140 , 15, 50, 30);
     
       
     
-    var col = ["serial", "n° Inventario","detalle"]//, "fecha", "transporte", "origen", "destino", "contacto","observaciones"] 
+    var col = ["cant.","serial", "n° Inventario","detalle","marca"]//, "fecha", "transporte", "origen", "destino", "contacto","observaciones"] 
     this.remito.forEach(element =>{
-      var temp = [element.nSerie,element.nInventario, element.Detalle]//, element.Fecha.toString().substring(0,10), element.Transporta, element.De, 
+      var temp = [element.Cantidad, element.nSerie,element.nInventario, element.Detalle, element.Marcas]//, element.Fecha.toString().substring(0,10), element.Transporta, element.De, 
       //element.Para, element.Contacto, element.Observaciones]
       cuerpo.push(temp)
     })
-    doc.autoTable(col,cuerpo,{startY:55})
-    let finalY = doc.previousAutoTable.finalY+20; //this gives you the value of the end-y-axis-position of the previous autotable.
+    doc[i].autoTable(col,cuerpo,{startY})
+    let finalY = doc[i].previousAutoTable.finalY+20; //this gives you the value of the end-y-axis-position of the previous autotable.
         
     if(finalY>210){
-      doc.addPage();
+      doc[i].addPage();
       finalY=20;
         
     }
     
-    doc.text('Total equipos: '+ items,10,finalY)
-    doc.text('_______________________________', 130, finalY + 10);
-    doc.text('Firma', 130, finalY + 15);
-    doc.text('_______________________________', 130, finalY + 30);
-    doc.text('Aclaración', 130, finalY + 35);
-    doc.text('_______________________________', 130, finalY + 50);
-    doc.text('DNI', 130, finalY + 55);
-    doc.text('_______________________________', 130, finalY + 70);
-    doc.text('Cargo', 130, finalY + 75);
-    doc.text('_______________________________', 10, finalY + 50);
-    doc.text('Entrego', 30, finalY + 55);
+    doc[i].text('Total equipos: '+ items,10,finalY)
+    doc[i].text('_______________________________', 130, finalY + 10);
+    doc[i].text('Firma', 130, finalY + 15);
+    doc[i].text('_______________________________', 130, finalY + 30);
+    doc[i].text('Aclaración', 130, finalY + 35);
+    doc[i].text('_______________________________', 130, finalY + 50);
+    doc[i].text('DNI', 130, finalY + 55);
+    doc[i].text('_______________________________', 130, finalY + 70);
+    doc[i].text('Cargo', 130, finalY + 75);
+    doc[i].text('_______________________________', 10, finalY + 50);
+    doc[i].text('Entrego', 30, finalY + 55);
     
 
     
-    //doc.addImage(imgData, 'png',125 , finalY + 20, 50, 30)
-    const pageCount = doc.internal.getNumberOfPages();
+    //doc[i].addImage(imgData, 'png',125 , finalY + 20, 50, 30)
+    const pageCount = doc[i].internal.getNumberOfPages();
 
-// For each page, print the page number and the total pages
-    for(var i = 1; i <= pageCount; i++) 
+    // For each page, print the page number and the total pages
+    for(var j = 1; j <= pageCount; j++) 
     {
       // Go to page i
-      doc.setPage(i);
+      doc[i].setPage(j);
       //Print Page 1 of 4 for example
-      doc.text('Pagina ' + String(i) + ' de ' + String(pageCount),210-20,290,null,null,"right");
+      doc[i].text('Pagina ' + String(j) + ' de ' + String(pageCount),210-20,290,null,null,"right");
     }
     
-   
-    doc.save('acta Nº '+this.nRemito);
+    var ver:string;
+    //for (var i=0;i<versiones.length;i++){
+      ver='';
+      ver=versiones[i];
+      //console.log(ver+' '+versiones.length);
+      
+      doc[i].setFontSize(25);
+      doc[i].text(ver,60,10); 
+      
+      doc[i].save('acta Nº '+this.nRemito+' '+ver);
+      //doc[i].clean();
+      
+    }
   }
-        //esto es lo que estaba para que quede mas lindo pero no continua en una hoja nueva
-        // var cuerpo = [];
-        // var imgData = "../../assets/logo13.png"
-        // var width = doc.internal.pageSize.getWidth();
-        // var height = doc.internal.pageSize.getHeight();   
-        // var col = ["serial", "detalle"] 
-        // this.remito.forEach(element =>{
-        //   var temp = [element.nSerie, element.Detalle]
-        //   cuerpo.push(temp)
-        // })
-        // doc.autoTable(col,cuerpo)
-        // let finalY = doc.previousAutoTable.finalY;
-        // doc.setFontSize(12).text('Acta Nº: '+this.nRemito, 15, finalY + 10);
-        // doc.text('Fecha: '+this.fecha.toString().substring(0,10), 100, finalY + 10);
-        // doc.text('Transporte: '+this.transporte, 200, finalY + 10);
-        // doc.text('Origen: '+this.origen, 15, finalY + 20);
-        // doc.text('Destino: '+this.destino, 15, finalY + 30);
-        // doc.text('Contacto: '+this.contacto, 15, finalY + 40);
-        // doc.text('Observacion: '+this.observaciones , 15, finalY + 50, {maxWidth: 260, align: "justify"});
-        // doc.addPage()
 
-        // doc.addImage(imgData, 'png',120 , finalY + 60, 50, 30)
- 
-        //esto es para el que me paso luis que borre los npm
-  // async pdf(){
-
-  //   let fecha = this.fecha.toString().substring(0,10)
-  //   const pdf = new PdfMakeWrapper();
-  //   pdf.pageSize('A4');
-  //   pdf.pageMargins([ 60, 60]);
-
-  //   pdf.background(new Txt('TC ordenado').alignment("center").end)
-       
-  //   pdf.add(await new Img('assets/logo13.png').alignment("center").height(80).width(135).build())
-
-  //   pdf.add([
-  //     new Txt('Acta Nº '+ this.nRemito  ).bold().italics().fontSize(20).end, 
-  //     new SVG('<svg width="20" height="15" viewBox="0 0 20 15">...</svg>').end,
-  //     new Columns([ 'Fecha: '+fecha+ '', 'Transporte: '+this.transporte+'' ]).columnGap(10).end,
-  //     new SVG('<svg width="20" height="15" viewBox="0 0 20 15">...</svg>').end,
-  //     new Txt('Origen: '+this.origen).end,
-  //     new SVG('<svg width="20" height="15" viewBox="0 0 20 15">...</svg>').end,
-  //     new Txt('Destino: '+this.destino).end,
-  //     new SVG('<svg width="20" height="15" viewBox="0 0 20 15">...</svg>').end,
-  //     new Txt('Contacto: '+this.contacto).end,
-  //     new SVG('<svg width="20" height="15" viewBox="0 0 20 15">...</svg>').end,
-  //     new Txt('Observaciones: '+this.observaciones).end,
-  //     new SVG('<svg width="20" height="15" viewBox="0 0 20 15">...</svg>').end,
-  //   ])
-  //   // this.remito.forEach(element => {
-  //   //   new Columns([ 'Serial: '+element.nSerie+ '', 'Detalle: '+element.Detalle ]).columnGap(10).end
-  //   // }),
-      
-  //   pdf.add([
-      
-  //   ])
-   
-    
-
-    
-  
-  //  // pdf.add(new Columns([ 'Hello', 'world' ]).columnGap(10).end)
-  //   pdf.footer("pie de pag")
-  //   pdf.create().open()
-  // }
 }
